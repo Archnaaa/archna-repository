ec7c502e84b0dd9a05a5df57c220001741776bb8
Archnaaa <1040119825@qq.com>
2022-03-07 11:11:55 +0800

    ok

---
+++ src/wk/ShowLogicErrors.java
@@ -0,0 +1,11 @@
+package wk;
+
+public class ShowLogicErrors {
+
+	public static void main(String[] args) {
+		System.out.print("Celsius 35 is Fahrenheit degree ");
+		System.out.println((9/5)*35+32);
+
+	}
+
+}
---
+++ src/wk/ShowSyntaxErrors.java
@@ -0,0 +1,10 @@
+package wk;
+
+public class ShowSyntaxErrors {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java");
+
+	}
+
+}
